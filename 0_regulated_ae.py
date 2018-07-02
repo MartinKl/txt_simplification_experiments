9991f7fd80893db42ecd608686488f0b9e7f03c7
Martin Klotz <MartinKl@hu-berlin.de>
2018-07-02 22:55:11 +0200

    encoding works again

--- 0_regulated_ae.py
+++ 0_regulated_ae.py
@@ -7,6 +7,7 @@ import os
 
 V = 1000
 EMB_D = 64
+D = 96
 LR = .1
 CLIP_NORM = 5.
 BS = 1
@@ -17,31 +18,45 @@ x_normal = tf.placeholder(tf.int32, shape=(None, LEN,), name='normal')
 x_simple = tf.placeholder(tf.int32, shape=(None, LEN,), name='simple')
 
 initial_encoder_state = [
-    (tf.zeros((BS, 96,), dtype=np.float32), tf.zeros((BS, 96,), dtype=np.float32)),
-    (tf.zeros((BS, 96,), dtype=np.float32), tf.zeros((BS, 96,), dtype=np.float32))
+    LSTMStateTuple(tf.zeros((BS, D,), dtype=tf.float32), tf.zeros((BS, D,), dtype=tf.float32)),
+    LSTMStateTuple(tf.zeros((BS, D,), dtype=tf.float32), tf.zeros((BS, D,), dtype=tf.float32))
 ]
 
 with tf.variable_scope('embedding') as scope:
-    embedding_n = tf.contrib.layers.embed_sequence(x_normal, vocab_size=V, embed_dim=EMB_D)
-    print('1:', tf.global_variables())
-    embedding_s = tf.contrib.layers.embed_sequence(x_simple, vocab_size=V, embed_dim=EMB_D, reuse=True, scope=scope)
-    print('2:', tf.global_variables())
+    embedded_n = tf.contrib.layers.embed_sequence(x_normal, vocab_size=V, embed_dim=EMB_D, scope=scope)
+    embedded_s = tf.contrib.layers.embed_sequence(x_simple, reuse=True, scope=scope)
 
 with tf.variable_scope('encoder'):
-    encoder = MultiRNNCell([LSTMCell(96), LSTMCell(96)])
-with tf.variable_scope('encode_n'):
-    z_normal, _ = encoder(tf.to_float(x_normal, name='normal_to_float'), initial_encoder_state)
-with tf.variable_scope('encode_s'):
-    z_simple, _ = encoder(tf.to_float(x_simple, name='simple_to_float'), initial_encoder_state)
+    encoder = MultiRNNCell([LSTMCell(D), LSTMCell(D)])
+
+with tf.variable_scope('encode_n') as scope:
+    z_normal_arr = tf.TensorArray(dtype=tf.float32, size=LEN)
+    z, state = encoder(embedded_n[:, 0], initial_encoder_state)
+    z_normal_arr.write(0, z)
+with tf.variable_scope(scope, reuse=True):
+    for i in range(1, LEN):
+        z, state = encoder(embedded_n[:, i], state)
+        z_normal_arr.write(i, z)
+
+with tf.variable_scope('encode_s') as scope:
+    z_simple_arr = tf.TensorArray(dtype=tf.float32, size=LEN)
+    z, state = encoder(embedded_s[:, 0], initial_encoder_state)
+    z_simple_arr.write(0, z)
+with tf.variable_scope(scope, reuse=True):
+    for i in range(1, LEN):
+        z, state = encoder(embedded_s[:, i], state)
+        z_simple_arr.write(i, z)
+
+exit()
 
 initial_decoder_state = [
-    LSTMStateTuple(tf.zeros((BS, 96,), dtype=tf.float32), tf.zeros((BS, 96,), dtype=tf.float32)),
-    LSTMStateTuple(tf.zeros((BS, 96,), dtype=tf.float32), tf.zeros((BS, 96,), dtype=tf.float32)),
+    LSTMStateTuple(tf.zeros((BS, D,), dtype=tf.float32), tf.zeros((BS, D,), dtype=tf.float32)),
+    LSTMStateTuple(tf.zeros((BS, D,), dtype=tf.float32), tf.zeros((BS, D,), dtype=tf.float32)),
     LSTMStateTuple(tf.zeros((BS, V,), dtype=tf.float32), tf.zeros((BS, V,), dtype=tf.float32))
 ]
 
 with tf.variable_scope('decode_n') as scope:
-    decoder_n = MultiRNNCell([LSTMCell(96), LSTMCell(96), LSTMCell(V)])
+    decoder_n = MultiRNNCell([LSTMCell(D), LSTMCell(D), LSTMCell(V)])
     state = initial_decoder_state
     logits_normal = []
     logits, state = decoder_n(z_normal, state)
@@ -55,7 +70,7 @@ with tf.variable_scope(scope, reuse=True):
                                  weights=tf.ones_like(x_normal, dtype=tf.float32))
 
 with tf.variable_scope('decode_s') as scope:
-    decoder_s = MultiRNNCell([LSTMCell(96), LSTMCell(96), LSTMCell(V)])
+    decoder_s = MultiRNNCell([LSTMCell(D), LSTMCell(D), LSTMCell(V)])
     state = initial_decoder_state
     logits_simple = []
     logits, state = decoder_s(z_simple, state)
