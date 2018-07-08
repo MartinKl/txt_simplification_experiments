fa6dc8116db0e3b4e357cfa558461bcb91ed8264
Martin Klotz <MartinKl@hu-berlin.de>
2018-07-08 15:08:32 +0200

    working model, but consumes A LOT OF memory

--- 0_regulated_ae.py
+++ 0_regulated_ae.py
@@ -11,11 +11,11 @@ with open('bin_data/uniwiki/dict_reduced.bin', 'rb') as f:
     vocab = pickle.load(f)
 eos_ix = len(vocab)
 v = len(vocab) + 1
-EMB_D = 64
-D = 96
+EMB_D = 16
+D = 8
 LR = .1
 CLIP_NORM = 5.
-BS = 32
+BS = 1
 LEN = 34
 
 # model definition
@@ -86,9 +86,9 @@ with tf.variable_scope(scope, reuse=True):
     for _ in range(1, LEN):
         logits, state = decoder_n(z_normal, state)
         logits_normal.append(logits)
-    err_r_normal = sequence_loss(logits=tf.convert_to_tensor(logits_normal),
-                                 targets=x_normal,
-                                 weights=w_normal)
+    err_r_normal = tf.reduce_sum(sequence_loss(tf.transpose(tf.convert_to_tensor(logits_normal), (1, 0, 2)),
+                                               x_normal,
+                                               w_normal))
 
 with tf.variable_scope('decode_s') as scope:
     decoder_s = MultiRNNCell([LSTMCell(D), LSTMCell(D), LSTMCell(v)])
@@ -100,12 +100,12 @@ with tf.variable_scope(scope, reuse=True):
     for _ in range(1, LEN):
         logits, state = decoder_s(z_simple, state)
         logits_simple.append(logits)
-    err_r_simple = sequence_loss(logits=tf.convert_to_tensor(logits_simple),
-                                 targets=x_simple,
-                                 weights=w_simple)
+    err_r_simple = tf.reduce_sum(sequence_loss(tf.transpose(tf.convert_to_tensor(logits_simple), (1, 0, 2)),
+                                               x_simple,
+                                               w_simple))
 
 err_z = tf.reduce_sum(tf.squared_difference(z_simple, z_normal), name='repr_error')
-err = err_r_normal + err_r_simple + err_z
+err = err_z + err_r_normal + err_r_simple
 
 params = tf.trainable_variables()
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=LR)
@@ -115,30 +115,17 @@ update = optimizer.apply_gradients(grads_and_vars=zip(clipped_gradients, params)
 
 # load data
 print('loading data ...')
-with open('bin_data/uniwiki/data_reduced.bin', 'rb') as f:
-    data = pickle.load(f)
-normal = []
-normal_w = []
-simple = []
-simple_w = []
-for example_n, example_s in zip(data['normal'], data['simple']):
-    normal.append(np.array(example_n + [eos_ix] + [0] * (LEN - len(example_n) - 1)))
-    normal_w.append(np.array(normal[-1] > 0).astype(np.float32))
-    simple.append(np.array(example_s + [eos_ix] + [0] * (LEN - len(example_s) - 1)))
-    simple_w.append(np.array(simple[-1] > 0).astype(np.float32))
-normal = np.array(normal, dtype=np.int32)
-simple = np.array(simple, dtype=np.int32)
-normal_w = np.array(normal_w, dtype=np.float32)
-simple_w = np.array(simple_w, dtype=np.float32)
-data = None
-gc.collect()
+normal = np.load('bin_data/uniwiki34/normal.npy')
+normal_w = np.load('bin_data/uniwiki34/w_normal.npy')
+simple = np.load('bin_data/uniwiki34/simple.npy')
+simple_w = np.load('bin_data/uniwiki34/w_simple.npy')
+
 # training
 with tf.Session() as session:
     print('Inititalizing ...')
     session.run(tf.global_variables_initializer())
     print('Starting training ...')
     epoch = 0
-    step = 0
     while True:
         epoch += 1
         print('Starting epoch', epoch, '...')
@@ -146,9 +133,8 @@ with tf.Session() as session:
             if not batch_index / BS % 5:
                 print('Feeding batch', 1 + batch_index // BS)
             result = session.run(update, feed_dict={
-                'normal:{}'.format(step): normal[batch_index:batch_index + BS],
-                'simple:{}'.format(step): simple[batch_index:batch_index + BS],
-                'nweights:{}'.format(step): normal_w[batch_index:batch_index + BS],
-                'sweights:{}'.format(step): simple_w[batch_index:batch_index + BS]
+                'normal:0': normal[batch_index:batch_index + BS],
+                'simple:0': simple[batch_index:batch_index + BS],
+                'nweights:0': normal_w[batch_index:batch_index + BS],
+                'sweights:0': simple_w[batch_index:batch_index + BS]
             })
-            step += 1
