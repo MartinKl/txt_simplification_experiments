35c87ff2a11d402a3c4722e8ac4b035a0af280f4
Martin Klotz <MartinKl@hu-berlin.de>
2018-10-13 01:54:00 +0200

    test

--- run_ae.py
+++ run_ae.py
@@ -2,9 +2,9 @@ from lib.data import DataCollection
 from lib.model import AE, ModelParameters, TrainingParameters
 import numpy as np
 
-normal = np.load('bin_data/uniwiki34/normal.npy')
+normal = np.load('bin_data/uniwiki34/normal.npy').clip(max=10)
 normal_w = np.load('bin_data/uniwiki34/w_normal.npy')
-simple = np.load('bin_data/uniwiki34/simple.npy')
+simple = np.load('bin_data/uniwiki34/simple.npy').clip(max=10)
 simple_w = np.load('bin_data/uniwiki34/w_simple.npy')
 
 train_set = (0, int(.8 * normal.shape[0]))
