555deffe115f3da05bc97c4d38451d68afa63727
Martin Klotz <MartinKl@hu-berlin.de>
2018-10-15 21:34:53 +0200

    eval script

--- eval.py
+++ eval.py
@@ -3,10 +3,12 @@ from lib.data import DataCollection
 from lib.model import AE, DiscriminatorModel, ModelParameters, TrainingParameters
 import numpy as np
 import os
+import pickle
 
 parser = ArgumentParser()
 parser.add_argument('directory', type=str, help='training and log dir')
 parser.add_argument('model_type', type=str)
+parser.add_argument('out_dir', type=str)
 args = parser.parse_args()
 
 normal = np.load('bin_data/uniwiki_minimal/normal.npy')
@@ -26,12 +28,42 @@ print(training_params)
 data = DataCollection(normal, simple, normal_w, simple_w)
 model_type = eval(args.model_type)
 print('Model type:', model_type.__name__)
+predictions_normal = ([], [])
+predictions_simple = ([], [])
+z_normal = ([], [])
+z_simple = ([], [])
+d_vals = ([], [])
+run_again = False
+kwargs = {}
+if model_type == DiscriminatorModel:
+    run_again = True
 with model_type(training_params=training_params,
                 model_params=model_params,
                 clean_environment=True,
                 load=True,
                 auto_save=False) as model:
-    for batch_index, batch in data['test'].batches(batch_size=training_params.batch_size):
-        values = model.predict(*batch)
-        print(*[np.array(v).shape for v in values], sep=os.linesep)
-        break
+    for r in range(1 + run_again):
+        for batch_index, batch in data['test'].batches(batch_size=training_params.batch_size):
+            if not batch_index % 50:
+                print('Current batch index:', batch_index)
+            if batch.shape[1] != 32:
+                print('end!')
+                break
+            values = [np.array(v) for v in model.predict(*batch, **kwargs)]
+            predictions_normal[r].append(values[0])
+            predictions_simple[r].append(values[1])
+            z_normal[r].append(values[2])
+            z_simple[r].append(values[3])
+            if run_again:
+                d_vals[r].append(values[4])
+        if run_again:
+            print('running again with theta=1')
+            kwargs = dict(tc=1.)
+
+if not os.path.exists(args.out_dir):
+    os.mkdir(args.out_dir)
+np.save(os.path.join(args.out_dir, 'normal_logits.npy'), np.array(predictions_normal))
+np.save(os.path.join(args.out_dir, 'simple_logits.npy'), np.array(predictions_simple))
+np.save(os.path.join(args.out_dir, 'z_normal.npy'), np.array(z_normal))
+np.save(os.path.join(args.out_dir, 'z_simple.npy'), np.array(z_simple))
+np.save(os.path.join(args.out_dir, 'd_outs.npy'), np.array(d_outs))
