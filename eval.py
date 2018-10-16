bd10b6b2dc9db99ffd13b7cbd180ff48ede8c864
Martin Klotz <MartinKl@hu-berlin.de>
2018-10-16 05:05:26 +0200

    finished

--- eval.py
+++ eval.py
@@ -1,4 +1,5 @@
-from bokeh.charts import HeatMap, output_file, Line
+from bokeh.charts import HeatMap, output_file
+from bokeh import palettes
 from bokeh.plotting import show, figure
 from lib.data import DataCollection
 from nltk.translate.bleu_score import sentence_bleu
@@ -36,33 +37,56 @@ p.line(x[2000:], d_outs_1[2000:], color='red')
 ## evaluate z
 z_normal = np.load(os.path.join(DSC_DIR, 'z_' + NORMAL_NAME))[0].reshape(134 * 32, 64)
 z_simple = np.load(os.path.join(DSC_DIR, 'z_' + SIMPLE_NAME))[0].reshape(134 * 32, 64)
-print(z_normal.flatten())
 
+select_0 = 100
 output_file('heat_map_dsc.html')
-sq_diff = ((z_normal - z_simple) ** 2).flatten()
-y = np.array([list(range(64))] * 4288).flatten()
-x = np.array([[i] * 64 for i in range(4288)]).flatten()
-show(HeatMap({'values': sq_diff, 'z[i]': x, 'example': y},
-             x='z[i]',
-             y='example',
+sq_diff = ((z_normal - z_simple)[:select_0] ** 2).flatten()
+y = np.array([list(range(z_normal.shape[1]))] * select_0).flatten()
+x = np.array([[i] * z_normal.shape[1] for i in range(select_0)]).flatten()
+pl = HeatMap({'values': sq_diff, 'example': x, 'z[i]': y},
+             y='z[i]',
+             x='example',
              values='values',
              stat=None,
-             title='squared difference of z along the test set (implicit model)'))
+             palette=palettes.Spectral9)
+pl.title.text_font_size = '18pt'
+pl.legend.location = 'bottom_right'
+pl.legend.label_text_font_size = '12pt'
+pl.xaxis.major_label_text_font_size = '14pt'
+pl.yaxis.major_label_text_font_size = '14pt'
+pl.xaxis.axis_label_text_font_size = '14pt'
+pl.yaxis.axis_label_text_font_size = '14pt'
+show(pl)
 output_file('normal_z_heat_map_dsc.html')
-show(HeatMap({'values': z_normal.flatten(), 'z[i]': x, 'example': y},
-             x='z[i]',
-             y='example',
+pl = HeatMap({'values': z_normal[:select_0].flatten(), 'z[i]':y, 'example': x},
+             y='z[i]',
+             x='example',
              values='values',
              stat=None,
-             title='normal z-values along the test set (implicit model)'))
+             palette=palettes.Spectral9)
+pl.title.text_font_size = '18pt'
+pl.legend.location = 'bottom_right'
+pl.legend.label_text_font_size = '12pt'
+pl.xaxis.major_label_text_font_size = '14pt'
+pl.yaxis.major_label_text_font_size = '14pt'
+pl.xaxis.axis_label_text_font_size = '14pt'
+pl.yaxis.axis_label_text_font_size = '14pt'
+show(pl)
 output_file('simple_z_heat_map_dsc.html')
-show(HeatMap({'values': z_simple.flatten(), 'z[i]': x, 'example': y},
-             x='z[i]',
-             y='example',
+pl = HeatMap({'values': z_simple[:select_0].flatten(), 'z[i]': y, 'example': x},
+             y='z[i]',
+             x='example',
              values='values',
              stat=None,
-             title='simple z-values along the test set (implicit model)'))
-
+             palette=palettes.Spectral9)
+pl.title.text_font_size = '18pt'
+pl.legend.location = 'bottom_right'
+pl.legend.label_text_font_size = '12pt'
+pl.xaxis.major_label_text_font_size = '14pt'
+pl.yaxis.major_label_text_font_size = '14pt'
+pl.xaxis.axis_label_text_font_size = '14pt'
+pl.yaxis.axis_label_text_font_size = '14pt'
+show(pl)
 
 ## evaluate predictions with bleu score
 normal = np.load(os.path.join(DSC_DIR, NORMAL_NAME))[0].swapaxes(1, 2).reshape(134 * 32, 34)
@@ -94,27 +118,51 @@ z_normal = np.load(os.path.join(AE_DIR, 'z_' + NORMAL_NAME)).reshape(134 * 32, 6
 z_simple = np.load(os.path.join(AE_DIR, 'z_' + SIMPLE_NAME)).reshape(134 * 32, 64)
 
 output_file('heat_map_ae.html')
-sq_diff = ((z_normal - z_simple) ** 2).flatten()
-show(HeatMap({'values': sq_diff, 'z[i]': x, 'example': y},
-             x='z[i]',
-             y='example',
+sq_diff = ((z_normal - z_simple)[:select_0] ** 2).flatten()
+pl = HeatMap({'values': sq_diff, 'z[i]': y, 'example': x},
+             y='z[i]',
+             x='example',
              values='values',
              stat=None,
-             title='squared difference of z along the test set (explicit model)'))
+             palette=palettes.Spectral9)
+pl.title.text_font_size = '18pt'
+pl.legend.location = 'bottom_right'
+pl.legend.label_text_font_size = '12pt'
+pl.xaxis.major_label_text_font_size = '14pt'
+pl.yaxis.major_label_text_font_size = '14pt'
+pl.xaxis.axis_label_text_font_size = '14pt'
+pl.yaxis.axis_label_text_font_size = '14pt'
+show(pl)
 output_file('normal_z_heat_map_ae.html')
-show(HeatMap({'values': z_normal.flatten(), 'z[i]': x, 'example': y},
-             x='z[i]',
-             y='example',
+pl = HeatMap({'values': z_normal[:select_0].flatten(), 'z[i]': y, 'example': x},
+             y='z[i]',
+             x='example',
              values='values',
              stat=None,
-             title='normal z-values along the test set (explicit model)'))
+             palette=palettes.Spectral9)
+pl.title.text_font_size = '18pt'
+pl.legend.location = 'bottom_right'
+pl.legend.label_text_font_size = '12pt'
+pl.xaxis.major_label_text_font_size = '14pt'
+pl.yaxis.major_label_text_font_size = '14pt'
+pl.xaxis.axis_label_text_font_size = '14pt'
+pl.yaxis.axis_label_text_font_size = '14pt'
+show(pl)
 output_file('simple_z_heat_map_ae.html')
-show(HeatMap({'values': z_simple.flatten(), 'z[i]': x, 'example': y},
-             x='z[i]',
-             y='example',
+pl = HeatMap({'values': z_simple[:select_0].flatten(), 'z[i]': y, 'example': x},
+             y='z[i]',
+             x='example',
              values='values',
              stat=None,
-             title='simple z-values along the test set (explicit model)'))
+             palette=palettes.Spectral9)
+pl.title.text_font_size = '18pt'
+pl.legend.location = 'bottom_right'
+pl.legend.label_text_font_size = '12pt'
+pl.xaxis.major_label_text_font_size = '14pt'
+pl.yaxis.major_label_text_font_size = '14pt'
+pl.xaxis.axis_label_text_font_size = '14pt'
+pl.yaxis.axis_label_text_font_size = '14pt'
+show(pl)
 
 ## evaluate predictions with bleu score
 normal = np.load(os.path.join(AE_DIR, NORMAL_NAME)).swapaxes(2, 3).reshape(134 * 32, 34)
